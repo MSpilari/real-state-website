556cc1fa86b0d060b62913a92356ce1abcdab69b
MSpilari <mb.spilari@hotmail.com>
2024-10-01 11:09:41 -0300

    style: augment colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,35 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        purple: {
+          60: "#703BF7",
+          65: "#8254F8",
+          70: "#946CF9",
+          75: "#A685FA",
+          90: "#DBCEFD",
+          95: "#EDE7FE",
+          97: "#F4F0FE",
+          99: "#FBFAFF",
+        },
+        grey: {
+          8: "#141414",
+          10: "#1A1A1A",
+          15: "#262626",
+          20: "#333333",
+          30: "#4D4D4D",
+          40: "#666666",
+          50: "#808080",
+          60: "#999999",
+        },
+      },
+      fontFamily: {
+        urbanist: ["var(--font-urbanist)"],
       },
     },
   },
