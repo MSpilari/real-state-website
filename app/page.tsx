f6bae063fb7a3eacdc274d43f8ce42cac6080267
MSpilari <mb.spilari@hotmail.com>
2024-10-01 18:01:58 -0300

    feat: add Hero and InfoCards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@/src/components/Header";
+import { Hero } from "@/src/components/Hero";
+import { InfoCards } from "@/src/components/InfoCards";
 
 export default function Home() {
   return (
     <main className="font-urbanist">
       <Header />
-      <section>Hero</section>
+      <Hero />
+      <InfoCards />
       <section>Properties</section>
       <section>Clients</section>
       <section>Start Banner</section>
